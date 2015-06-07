c8afb868986941573d3d79fc3c010bfb36e04b36
William Kingsford <will.kingsford@gmail.com>
2015-06-07 19:20:51 -0400

    Added echo's to debug

--- sync-and-time.sh
+++ sync-and-time.sh
@@ -8,18 +8,22 @@ echo $! > /home/william-kingsford/Logs/iostat_pid.txt
 
 # upload files
 seaf-cli start
+echo "Starting sync..."
 seaf-cli sync -l b10d5f2a-9099-439a-abc4-dcdbfebf58e1 -s http://142.150.234.157:8001 -d /home/william-kingsford/SeaFileLibraries/ -u will.kingsford@gmail.com -p *hLO8GeH
 
 # run until sync is complete
+echo "About to check for sync completion..."
 while seaf-cli status > /home/william-kingsford/Logs/status.txt; [ $(awk 'END { print $(NF) }' /home/william-kingsford/Logs/status.txt) = "synchronized" ]
 do :;
 done
+echo "Sync completed"
 
 finish=$(($(date +%s%N)-$start))
 # end iostat process
 kill -9 `cat /home/william-kingsford/Logs/iostat_pid.txt`
 
 # desync library for future tests
+echo "Desyncing library"
 seaf-cli desync -d /home/william-kingsford/SeaFileLibraries/
 seaf-cli stop
 
