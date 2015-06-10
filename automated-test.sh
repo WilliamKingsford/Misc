d320c648cce8acae5af3186d6f98de121c82c2df
William Kingsford <william-kingsford@c158.csl.toronto.edu>
2015-06-10 14:07:02 -0400

    Fixed syntax error, now can find sync-and-time.sh script

--- automated-test.sh
+++ automated-test.sh
@@ -15,7 +15,7 @@ do
 	echo "Testing ${i} x 4kB files"
 	echo "${i} x 4kB completion time in nanoseconds" >> /home/william-kingsford/Logs/all-times.txt
 	python maketree.py $((i+1)) 1 0 $((i+1)) 0 4000 /home/william-kingsford/SeaFileLibraries/
-	./home/william-kingsford/Misc/sync-and-time.sh
+	/home/william-kingsford/Misc/sync-and-time.sh
 	mv /home/william-kingsford/Logs/iostat.txt /home/william-kingsford/Logs/StoredLogs/${i}x4000B.txt
 	rm /home/william-kingsford/Logs/iostat_pid.txt
 done
@@ -25,7 +25,7 @@ echo "" >> /home/william-kingsford/Logs/all-times.txt # newline
 echo "Testing 10000 x 10B files"
 echo "10000 x 10B completion time in nanoseconds" >> /home/william-kingsford/Logs/all-times.txt
 python maketree.py 10001 1 0 10001 0 10 /home/william-kingsford/SeaFileLibraries/
-./home/william-kingsford/Misc/sync-and-time.sh
+/home/william-kingsford/Misc/sync-and-time.sh
 mv /home/william-kingsford/Logs/iostat.txt Logs/StoredLogs/10000x10B.txt
 rm /home/william-kingsford/Logs/iostat_pid.txt
 echo "" >> /home/william-kingsford/Logs/all-times.txt # newline
@@ -37,7 +37,7 @@ do
 	echo "Testing 1000 x $((j * 4))kB files"
 	echo "1000 x $((j * 4))kB completion time in nanoseconds" >> /home/william-kingsford/Logs/all-times.txt
 	python maketree.py 1001 1 0 1001 0 $((j * 4000)) /home/william-kingsford/SeaFileLibraries/
-	./home/william-kingsford/Misc/sync-and-time.sh
+	/home/william-kingsford/Misc/sync-and-time.sh
 	mv /home/william-kingsford/Logs/iostat.txt Logs/StoredLogs/1000x$((j * 4000))B.txt
 	rm /home/william-kingsford/Logs/iostat_pid.txt
 done
