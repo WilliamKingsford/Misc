a322e4b54d3e574342437cee863ce2cd7fe5b001
William Kingsford <will.kingsford@gmail.com>
2015-06-16 10:23:54 -0400

    Script to finish SF-server build

---
+++ SF-server-build-install-test2.sh
@@ -0,0 +1,6 @@
+#  Build the server+components and stuff in tarball 
+
+cd ~/
+
+/home/dev/seafile/scripts/build/build-server.py --libsearpc_version=1.2.2 --ccnet_version=1.4.2 --seafile_version=4.1.2  --thirdpartdir=/home/dev/seahub_thirdpart --srcdir=/home/dev/seafile-sources --outputdir=/home/dev/seafile-server-pkgs --version=4.1.2 --builddir=/mnt/ --keep
+
