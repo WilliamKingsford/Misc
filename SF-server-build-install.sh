fa625ff16ad3827fd58eed27ca1906e09c0d7ce3
William Kingsford <will.kingsford@gmail.com>
2015-06-16 10:41:57 -0400

    Redid script based on http://forum.doozan.com/read.php?2,21772,21772

--- SF-server-build-install.sh
+++ SF-server-build-install.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#Seafile Build Instructions V2
+
+# starting w/ fresh rootfs of Wheezy (or Jessie)
+
+# most commands where executed in ~/  (in this case, /home/william-kingsford , homedir of a regular+sudoer user)
 
 # check if running as root
 if [ "$(id -u)" != "0" ]; then
@@ -6,128 +10,248 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-cd /home/william-kingsford/
-mkdir Downloads
-cd Downloads
+# install essential Debian packages
 
-# install seafile dependencies
-apt-get install libevent-dev libcurl4-openssl-dev libglib2.0-dev uuid-dev intltool libsqlite3-dev libmysqlclient-dev libarchive-dev libtool libjansson-dev valac libfuse-dev re2c flex cmake make g++ python-pip
-# dependencies that must be compiled from source
-wget http://www.tildeslash.com/libzdb/dist/libzdb-2.12.tar.gz
-tar xf libzdb-2.12.tar.gz
-cd libzdb-2.12
-./configure
-make
-make install
-cd ..
+apt-get install libevent-dev libcurl4-openssl-dev libglib2.0-dev uuid-dev intltool \
+ libsqlite3-dev libmysqlclient-dev libarchive-dev libtool libjansson-dev valac   \
+ libfuse-dev re2c flex python-setuptools cmake git build-essential python-simplejson \
+ python-imaging python-pip python-dev unzip
+ 
+# Want postgresql support (instead of sqlite or mysql? If so...
+#  sudo apt-get install postgresql postgresql-server-dev-all libpq-dev
 
-#wget https://github.com/ellzey/libevhtp/archive/develop.zip
-#unzip develop.zip -d libevhtp
-#cd libevhtp/libevhtp-master/
-
-#wget https://github.com/ellzey/libevhtp/archive/1.1.6.tar.gz
-#tar xf 1.1.6.tar.gz
-#cd libevhtp-1.1.6
-
+# set up easy_install for python2.7  (I'd like to avoid this... but... had to)
+#  wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+#  ... mistake was that I forgot sudo prior to easy_install cmd
+ 
+ 
+# djblets required python-imaging
+# maybe better to build django 1.5.12 rather than use Debian's 1.4.x
+# consider adding in postgresql support next time!
+ 
+ 
+ 
+ 
+ #  install libevhtp 
+ 
+cd ~/
 git clone https://www.github.com/haiwen/libevhtp.git
 cd libevhtp
-cmake -DEVHTP_DISABLE_SSL=ON -DEVHTP_BUILD_SHARED=ON
+cmake -DEVHTP_DISABLE_SSL=OFF -DEVHTP_BUILD_SHARED=ON .
 make
-make install
-# fix bug where onigposix.h isn't properly installed
-cp onigurama/onigposix.h /usr/local/include/onigposix.h
-cd ..
+sudo make install
 
-while true; do
-    read -p "Continue? [y/n]" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# missing header to fix "/usr/local/include/evhtp.h:11:23: fatal error: onigposix.h: No such file or directory"
+#  ... this error doesn't show up until much later in the build process, but this is the its origin
+sudo cp oniguruma/onigposix.h /usr/local/include/
 
-# install seahub dependencies
-pip install python-django sqlite3 python-simplejson PIL chardet gunicorn django-compressor==1.4 django-statisi18n==1.1.2 six python-dateutil
-easy_install djblets
-apt-get update
 
-cd /home/william-kingsford/SeaFileServer/
-mkdir src
-cd src
-# download and rename source archives
-export version=4.1.2
-alias wget='wget --content-disposition -nc'
-wget https://github.com/haiwen/libsearpc/archive/v3.0-latest.tar.gz
-mv v3.0-latest.tar.gz libsearpc-3.0-latest.tar.gz
-wget https://github.com/haiwen/ccnet/archive/v${version}-server.tar.gz
-mv v${version}-server.tar.gz ccnet-${version}-server.tar.gz
-wget https://github.com/haiwen/seafile/archive/v${version}-server.tar.gz
-mv v${version}-server.tar.gz seafile-${version}-server.tar.gz
-wget https://github.com/haiwen/seahub/archive/v${version}-server.tar.gz
-mv v${version}-server.tar.gz seahub-${version}-server.tar.gz
 
-# extract tarballs
-tar xf libsearpc-3.0-latest.tar.gz
-tar xf ccnet-${version}-server.tar.gz
-tar xf seafile-${version}-server.tar.gz
-# seahub is python scripts, so is installed differently
-cd /home/william-kingsford/SeaFileServer/
-tar xf /home/william-kingsford/SeaFileServer/src/seahub-${version}-server.tar.gz
-mv seahub-${version}-server seahub
 
-# build seafile server
-cd /home/william-kingsford/SeaFileServer/src/
+ #  install libzdb
 
-cd libsearpc-3.0-latest
+cd ~/
+git clone https://www.github.com/haiwen/libzdb.git
+cd libzdb
+chmod +x autogen.sh
 ./autogen.sh
 ./configure
 make
-make install
-cd ..
+sudo make install
 
-cd ccnet-${version}-server
+
+
+# update lib
+sudo ldconfig
+
+
+
+# Download tarballs for thirdpart components
+
+cd /tmp
+wget -O Django-1.5.12.tar.gz https://www.djangoproject.com/download/1.5.12/tarball/
+wget -O djblets-0.6.14.tar.gz http://downloads.reviewboard.org/releases/Djblets/0.6/Djblets-0.6.14.tar.gz
+wget -O gunicorn-0.16.1.tar.gz   http://pypi.python.org/packages/source/g/gunicorn/gunicorn-0.16.1.tar.gz
+wget -O flup-1.0.tar.gz  http://pypi.python.org/packages/source/f/flup/flup-1.0.tar.gz
+# wget -O chardet-1.0.tar.gz   https://pypi.python.org/packages/source/c/chardet/chardet-1.0.tar.gz
+#   unknown if 2.3.0 has any advantages over 1.0
+wget -O chardet-2.3.0.tar.gz https://pypi.python.org/packages/source/c/chardet/chardet-2.3.0.tar.gz
+wget --no-check-certificate -O python-dateutil-1.5.tar.gz  https://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz
+wget -O six-1.9.0.tar.gz  https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz                                     
+
+# make dir for thirdpart components
+mkdir -p ~/seahub_thirdpart
+cd ~/seahub_thirdpart/
+
+# I ran into a problem where I didn't have adequate space for the django to be built in tmp...
+#
+#  "error: Setup script exited with error: No space left on device"
+#
+#  had to resort to fixing /usr/lib/python2.6/dist-packages/setuptools/command/easy_install.py
+#  see http://stackoverflow.com/questions/7621103/easy-install-downloading-directory
+#  FIX = /usr/lib/python2.6/dist-packages/setuptools/command/easy_install.py
+#  find "tempfile.mkdtemp", change to something like below ... (/mnt is where I mounted sda3 ... lots of space)
+#              tmpdir = tempfile.mkdtemp(prefix="easy_install-",dir="/mnt")
+chmod 777 -R /mnt  # make writeable, so that python has lots of build space
+
+
+
+export PYTHONPATH=.
+easy_install -d . /tmp/Django-1.5.12.tar.gz
+easy_install -d . /tmp/djblets-0.6.14.tar.gz
+easy_install -d . /tmp/gunicorn-0.16.1.tar.gz
+easy_install -d . /tmp/flup-1.0.tar.gz
+# easy_install -d . /tmp/chardet-1.0.tar.gz
+easy_install -d . /tmp/chardet-2.3.0.tar.gz
+easy_install -d . /tmp/python-dateutil-1.5.tar.gz
+easy_install -d . /tmp/six-1.9.0.tar.gz
+
+# unzip 2 of the .egg files : otherwise an error was triggered during later build activity
+#unzip chardet-1.0-py2.7.egg
+#unzip flup-1.0-py2.7.egg
+# error doesn't seem to appear any more...
+
+cd  ~/
+
+
+
+
+
+#  set some paths for building stuff...
+export PKG_CONFIG_PATH=/home/william-kingsford/seafile/lib:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=/home/william-kingsford/libsearpc:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=/home/william-kingsford/ccnet:$PKG_CONFIG_PATH
+
+
+
+# prep a dir for seafile-sources
+mkdir ~/seafile-sources
+
+# clone and prepare Seafile source tarballs
+
+cd ~/
+git clone https://github.com/haiwen/libsearpc.git
+cd libsearpc
+git reset --hard v3.0-latest
 ./autogen.sh
-./configure --disable-client --enable-server   # `export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig` if libsearpc is not found
-make
-make install
-cd ..
+./configure
+make dist
+cp *.tar.gz ~/seafile-sources/
 
-cd seafile-${version}-server
+cd ~/
+git clone https://github.com/haiwen/ccnet.git
+cd ccnet
+git reset --hard v4.1.1-server
 ./autogen.sh
-./configure --disable-client --enable-server
+./configure
+make dist
+cp *.tar.gz ~/seafile-sources/
+
+
+
+
+cd ~/
+git clone https://github.com/haiwen/seafile.git
+cd seafile
+git reset --hard v4.1.1-server
+./autogen.sh
+./configure
+make dist
+cp *.tar.gz ~/seafile-sources/
+
+
+
+
+
+cd ~/
+git clone https://github.com/haiwen/seahub.git
+cd seahub
+git reset --hard v4.1.1-server
+sudo pip install -r requirements.txt    # did this to get around what looked like missing dependencies 
+                                        #  (debs _were_ installed, but not seemingly recognized)
+                                        # Pillow and Django were installed...
+./tools/gen-tarball.py --version=4.1.1 --branch=HEAD
+cp *.tar.gz ~/seafile-sources/
+
+
+
+
+
+cd ~/
+git clone https://github.com/haiwen/seafobj.git
+cd seafobj
+git reset --hard v4.1.1-server
+make dist
+cp *.tar.gz ~/seafile-sources/
+
+
+
+
+cd ~/
+git clone https://github.com/haiwen/seafdav.git
+cd seafdav
+git reset --hard v4.1.1-server
 make
-make install
-cd ..
+cp *.tar.gz ~/seafile-sources/
 
-# refresh system libraries cache
-ldconfig
 
-# configure seafile
-cd seafile-${version}-server/scripts/
-echo "REMEMBER TO SET CCNET PORT TO 10003"
+# Now, the seafile build system/script requires that a few of the sources are "tagged" with the same version number...
+# ...so we go to ~/seafile-sources, untar some of them, change the dir names to have the same version number & retar
+
+cd ~/seafile-sources/
+
+tar xzvf seafile-4.1.4.tar.gz
+mv seafile-4.1.4 seafile-4.1.1
+tar czvf seafile-4.1.1.tar.gz seafile-4.1.1
+
+#tar xzvf seahub-<version>.tar.gz
+#mv seahub-<version> seahub-4.1.1
+#tar czvf seahub-4.1.1.tar.gz seahub-4.1.1
+
+#  Build the server+components and stuff in tarball 
+
+cd ~/
+
+/home/william-kingsford/seafile/scripts/build/build-server.py --libsearpc_version=1.2.2 --ccnet_version=1.4.2 --seafile_version=4.1.1  --thirdpartdir=/home/william-kingsford/seahub_thirdpart --srcdir=/home/william-kingsford/seafile-sources --outputdir=/home/william-kingsford/seafile-server-pkgs --version=4.1.2 --builddir=/mnt/ --keep
+
+
+# ===BEGIN INSTALLATION===
+
+apt-get update
+
+mkdir ~/SeaFileServer
+cd ~/SeaFileServer
+
+cp /home/william-kingsford/seafile-server-pkgs/seafile-server_4.1.2_x86-64.tar.gz seafile-server_4.1.2_x86-64.tar.gz
+tar -xvf seafile-server_4.1.2_x86-64.tar.gz
+mkdir installed
+mv seafile-server_4.1.2_x86-64.tar.gz installed
+
+apt-get update
+apt-get install python2.7 python-setuptools python-imaging sqlite3
+
+cd seafile-server-4.1.2
 ./setup-seafile.sh
-cd ../../
 
-# deploy server
-apt-get install nginx python-flup
-service nginx start
+apt-get install nginx python-flup python-imaging
 
 iptables -I INPUT 1 -p tcp --dport 8001 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 8082 -j ACCEPT
-iptables -I INPUT 1 -p tcp --dport 10003 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 10001 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 12001 -j ACCEPT
 
-wget https://raw.githubusercontent.com/WilliamKingsford/Misc/master/nginx-config
-mv nginx-config /etc/nginx/sites-available/site
+cp ../nginx-config /etc/nginx/sites-available/site
+
+service nginx start
+
 ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled/site
 rm /etc/nginx/sites-enabled/default
 service nginx restart
 
 echo '"./seafile.sh start" to start seafile'
-echo '"./seahub.sh start-fastcgi 8001" to start seahub'
+echo '"./seahub.sh start 8001" to start seahub'
 echo "THESE MUST BE RUN AS ROOT"
 
 echo "Remember to work through the guide at http://manual.seafile.com/deploy/deploy_with_nginx.html"
 
-echo "Also make sure that the IP address in the nginx config file is correct and use port 10003 for ccnet"
+echo "Also make sure that the IP address in the nginx config file is correct"
