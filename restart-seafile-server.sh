29c5a5bb01a8b1ced484cfbb4c8b17076891a8a8
William Kingsford <william-kingsford@c157.csl.toronto.edu>
2015-07-10 14:08:48 -0400

    Restart-seafile-server.sh now does seafile garbage collection and clears pagecache during restart

--- restart-seafile-server.sh
+++ restart-seafile-server.sh
@@ -4,5 +4,7 @@ cd /home/william-kingsford/SeaFileServer/seafile-server-4.1.2/
 ./seafile.sh stop
 ./seahub.sh stop
 sleep 1
+./seaf-gc.sh
+sh -c 'echo 1 > /proc/sys/vm/drop_caches'
 ./seafile.sh start
 ./seahub.sh start 8001
