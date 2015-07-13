8c2bdc02260f9c9be70ef1b28253a19517151d6e
William Kingsford <william-kingsford@c158.csl.toronto.edu>
2015-07-13 17:26:25 -0400

    Fixed typo

--- top-iotop.sh
+++ top-iotop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # generate one report
-top -b -n 1 | grep 'seaf-daemon\|ccnet' > /home/william-kingsford/Logs/top.txt
-iotop -b -n 1 | grep 'seaf-daemon\|ccnet' > /home/william-kingsford/Logs/iotop.txt
+top -b -n 1 | grep 'seaf-daemon\|ccnet' >> /home/william-kingsford/Logs/top.txt
+iotop -b -n 1 | grep 'seaf-daemon\|ccnet' >> /home/william-kingsford/Logs/iotop.txt
