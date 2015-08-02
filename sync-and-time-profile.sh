fcc35877832a444a0eaadd4338e8ec73acbd79fe
William Kingsford <will.kingsford@gmail.com>
2015-08-03 11:38:41 +1200

    Finished profiling script so it reads file number & size from command line and generates the required files before testing

--- sync-and-time-profile.sh
+++ sync-and-time-profile.sh
@@ -6,13 +6,66 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "sync-and-time.sh: Starting seaf-cli"
+# test for valid arguments
+if [[ $# -eq 0 ]]
+then echo "Syntax:"
+echo "generating no files: sync-and-time-profile.sh 0"
+echo "generating files:    sync-and-time-profile.sh 1 <# of files> <size of files (B)>"
+exit
+fi
+if !( [[ $# -eq 1 ]] || [[ $# -eq 3 ]] )
+then echo "Invalid number of arguments."
+echo "Syntax:"
+echo "generating no files: sync-and-time-profile.sh 0"
+echo "generating files:    sync-and-time-profile.sh 1 <# of files> <size of files (B)>"
+exit
+fi
+if [[ $# -eq 1 ]] && ![[ $1 -eq 0 ]]
+then echo "Invalid arguments. If only one argument is given it must be 0 (generate no files)."
+exit
+fi
+if ( [[ $# -eq 3 ]] && [[ !$1 -eq 1 ]] ) || ( [[ $1 -eq 1 ]] && ![[ $# -eq 3 ]] )
+then echo "If the first argument is 1 then there must be 3 total arguments (1, number of files, size of files) and vice versa."
+exit
+fi
+
+echo "EMPTY FOLDER PRE-SYNC: Starting seaf-cli and giving it 3 seconds to load"
 seaf-cli start > /dev/null 2>&1&
-# start tracking with OProfile
-#nohup operf --pid `ps -ef | grep 'ccnet --daemon' | awk 'NR % 2 - 1 == 0' | awk '{ print $2 }'` > /home/william-kingsford/Logs/ccnet-operf.txt 2>&1&
-#echo $! > /home/william-kingsford/Logs/operf-ccnet_pid.txt
-#nohup operf --pid `ps -ef | grep 'seaf-daemon' | awk 'NR % 2 - 1 == 0' | awk '{ print $2 }'` > /home/william-kingsford/Logs/seaf-operf.txt 2>&1&
-#echo $! > /home/william-kingsford/Logs/operf-seaf_pid.txt
+sleep 3
+
+echo "Starting sync..."
+seaf-cli sync -l 4915ec59-c414-47d1-a14e-ed290339172b -s http://142.150.234.157:8001 -d /home/william-kingsford/SeaFileLibraries/ -u will.kingsford@gmail.com -p *hLO8GeH
+
+# run until sync is complete
+continue="1"
+sleep 5
+echo "About to check for sync completion (empty folder)..."
+while [ "$continue" -eq "1" ]
+do
+seaf-cli status > /home/william-kingsford/Logs/status.txt
+sleep 1
+if [[ $(awk 'END { print $(NF) }' /home/william-kingsford/Logs/status.txt) = "synchronized" ]]
+then continue="0"
+else echo "Checking..."
+fi
+done
+
+seaf-cli stop
+
+echo "Sync completed with empty folder, removing old gmon.out"
+
+# remove gmon.out produced by the pre-sync
+rm gmon.out
+
+# generate files for sync if first argument is 1
+if [[ $1 -eq 1 ]]
+then echo "Generating files" 
+python maketree.py $(($2+1)) 1 0 $(($2+1)) 0 $3 /home/william-kingsford/SeaFileLibraries/
+else echo "Test with no added files"
+fi
+
+echo "FILE SYNC: Starting seaf-cli"
+seaf-cli start > /dev/null 2>&1&
 
 start=$(date +%s%N)
 # start tracking detailed cpu/io data every 0.1 second, running in background
@@ -24,7 +77,6 @@ echo $! > /home/william-kingsford/Logs/free_pid.txt
 
 # upload files
 echo "Starting sync..."
-#seaf-cli sync -l 4915ec59-c414-47d1-a14e-ed290339172b -s http://142.150.234.157:8001 -d /home/william-kingsford/SeaFileLibraries/ -u will.kingsford@gmail.com -p *hLO8GeH
 
 # run until sync is complete
 continue="1"
@@ -33,8 +85,6 @@ while [[ "$continue" -eq "1" ]]
 do
 seaf-cli status > /home/william-kingsford/Logs/status.txt
 # write sync status to file to find when sync begins
-#date +%Y-%m-%d-%H-%M-%S-%N >> /home/william-kingsford/Logs/status-messages.txt
-#awk 'END { print $(NF) }' /home/william-kingsford/Logs/status.txt >> /home/william-kingsford/Logs/status-messages.txt
 sleep 0.1
 if [[ $(awk 'END { print $(NF) }' /home/william-kingsford/Logs/status.txt) = "synchronized" ]]
 then continue="0"
@@ -46,8 +96,6 @@ echo "Sync completed"
 
 finish=$(($(date +%s%N)-$start))
 # end operf, top-iotop and free processes
-#kill -15 `cat /home/william-kingsford/Logs/operf-ccnet_pid.txt`
-#kill -15 `cat /home/william-kingsford/Logs/operf-seaf_pid.txt`
 kill -15 `cat /home/william-kingsford/Logs/top-iotop_pid.txt`
 kill -15 `cat /home/william-kingsford/Logs/free_pid.txt`
 
