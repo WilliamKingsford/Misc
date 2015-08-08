91b1a40c1e4b518dff47c444eebdc0b9754fe71c
William Kingsford <will.kingsford@gmail.com>
2015-08-08 20:21:24 +1200

    Now renames each gmon.out to prevent it from being overwritten when server is restarted

--- restart-seafile-server.sh
+++ restart-seafile-server.sh
@@ -4,6 +4,8 @@ cd /home/william-kingsford/SeaFileServer/seafile-server-4.1.2/
 ./seafile.sh stop
 ./seahub.sh stop
 sleep 1
+# rename gmon.out so it's not overwritten
+mv gmon.out gmon.out-$(date +'%Y-%m-%d-%R')
 ./seaf-gc.sh
 sh -c 'echo 1 > /proc/sys/vm/drop_caches'
 ./seafile.sh start
