3932d0716a68203ade63ddfcf3dcb2db910a1887
William Kingsford <william-kingsford@c157.csl.toronto.edu>
2015-08-12 16:25:33 -0400

    Updated to pass GMON_OUT_PREFIX environment variable

--- restart-seafile-server.sh
+++ restart-seafile-server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /home/william-kingsford/SeaFileServer/seafile-server-4.1.2/
-./seafile.sh stop
-./seahub.sh stop
+GMON_OUT_PREFIX="mygmon" ./seafile.sh stop
+GMON_OUT_PREFIX="mygmon" ./seahub.sh stop
 sleep 1
 # rename gmon.out so it's not overwritten
 mv gmon.out gprof/gmon.out-$(date +'%Y-%m-%d-%R')
 ./seaf-gc.sh
 sh -c 'echo 1 > /proc/sys/vm/drop_caches'
-./seafile.sh start
-./seahub.sh start 8001
+GMON_OUT_PREFIX="mygmon" ./seafile.sh start
+GMON_OUT_PREFIX="mygmon" ./seahub.sh start 8001
