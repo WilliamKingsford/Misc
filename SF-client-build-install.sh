b9855efadbff161e4522e8d27bcc3a07c183b3a2
William Kingsford <will.kingsford@gmail.com>
2015-08-25 14:27:57 +1200

    Removed installing unneeded desktop application

--- SF-client-build-install.sh
+++ SF-client-build-install.sh
@@ -32,17 +32,6 @@ make
 sudo make install
 cd ..
 
-# install cmake to /opt/cmake
-wget http://www.cmake.org/files/v3.2/cmake-3.2.3-Linux-x86_64.sh
-chmod +x cmake-3.2.3-Linux-x86_64.sh
-./cmake-3.2.3-Linux-x86_64.sh
-
-cd seafile-client-${version}
-../cmake-3.2.3-Linux-x86_64/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX .
-make
-sudo make install
-cd ..
-
 # install seafile command line interface
 mkdir seafile-cli
 cd seafile-cli
